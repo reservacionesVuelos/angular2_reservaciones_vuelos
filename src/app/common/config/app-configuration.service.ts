097b69789a29abdc3f63a71f6306580f5769904e
Carlos Andres Sanchez Venegas <carlos.sanchez-v@mail.escuelaing.edu.co>
2018-04-29 14:38:01 -0500

    adding local backend, fixing style from date

--- src/app/common/config/app-configuration.service.ts
+++ src/app/common/config/app-configuration.service.ts
@@ -6,8 +6,8 @@ export class AppConfiguration {
   private config:String;
 
   constructor( ) {
-    //this.config = "http://localhost:8080";
-    this.config = "https://reservacionesvuelos.herokuapp.com";
+    this.config = "http://localhost:8080";
+    //this.config = "https://reservacionesvuelos.herokuapp.com";
   }
 
   get apiURL(): String {
